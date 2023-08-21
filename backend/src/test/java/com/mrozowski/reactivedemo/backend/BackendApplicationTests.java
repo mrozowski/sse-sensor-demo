a012b6621a84b674812aa00e7af8360bb42a235e
mrozowski <mrozowskion@gmail.com>
2023-08-21 23:00:10 +0700

    initial

---
+++ backend/src/test/java/com/mrozowski/reactivedemo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrozowski.reactivedemo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
